08f2c16fc170cbe0c9e6fd7bcadcbbfcaeb661e1
Matt LaGrandeur <mattlag@gmail.com>
2024-04-05 11:00:41 -0700

    Trying to get React DevTools to work

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, session } from 'electron';
+import os from 'node:os';
 import path from 'path';
 import {
 	FileInfo,
@@ -47,10 +48,14 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-	// installExtension(REACT_DEVELOPER_TOOLS)
-	// 	.then((name) => console.log(`Added Extension:  ${name}`))
-	// 	.catch((err) => console.log('An error occurred: ', err));
+app.whenReady().then(async () => {
+	// const extensionResult = await session.defaultSession.loadExtension(
+	// 	path.join(
+	// 		os.homedir(),
+	// 		'AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\5.0.2_0'
+	// 	)
+	// );
+	// console.log(extensionResult);
 
 	/*
 	 * IPC Handlers
